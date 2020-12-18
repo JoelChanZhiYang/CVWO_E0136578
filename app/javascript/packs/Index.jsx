fcd4c52f57c8065e8fbd8a29fa3e61ceb257f294
Joel Chan <joelchanzhiyang@gmail.com>
2020-12-18 21:49:24 +0800

    hi

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -7,9 +7,7 @@ import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-// import 'bootstrap'
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
-// import '../stylesheets/application.scss'
+import '.../app/stylesheets/application'
 import App from "../components/App";
 
 console.log("hihihi")
